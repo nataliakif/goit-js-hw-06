70fbbc05b68525e7bc7576381235a2ef0629e9e3
nataliakif <nataliakif@gmail.com>
2022-01-26 11:39:59 +0200

    2nd task

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const categoriesAmount = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesAmount.length}`);
 const itemsAmount = document.querySelectorAll('.item');
 itemsAmount.forEach(element =>{
-    const categoryTitle = element.querySelectorAll("h2")
-    categoryTitle.forEach(title => console.log("category:"+ ' ' + title.textContent));
-    const listLength = element.querySelectorAll('ul li');
-    console.log("Elements:" + " " + listLength.length)
+    const categoryTitle = element.firstElementChild.textContent;
+    console.log("category:"+ ' ' + categoryTitle);
+    const listLength = element.lastElementChild.children.length;
+    console.log("Elements:" + " " + listLength)
 });
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+const makeIngredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+    const newItem = document.createElement('li');
+    newItem.textContent = ingredient;
+    newItem.classList.add('item');
+    return newItem;
+  });
+}
+const elements = makeIngredientsList(ingredients);
+ingredientsList.append(...elements);
