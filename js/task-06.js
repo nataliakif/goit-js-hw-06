ef7ad4d641e5d5cbc34e0a88ad3da535684f44e8
nataliakif <nataliakif@gmail.com>
2022-01-27 20:48:31 +0200

    task 8

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@
    const inputEl = document.querySelector('#validation-input');
    const attributeValue = inputEl.getAttribute('data-length');
 
- inputEl.addEventListener('blur', onInputBlur, true);
+ inputEl.addEventListener('blur', onInputBlur);
     function onInputBlur(event){
-        if(event.currentTarget.value.length == attributeValue){
+        if(event.currentTarget.value.length === attributeValue){
             return inputEl.classList.add("valid")
         }    
         inputEl.classList.add("invalid")
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const refs={
+    inputEmail:  document.querySelector('[name="email"]'),
+    inputPass:  document.querySelector('[name="password"]'),
+    loginForm:  document.querySelector('.login-form'),
+}
+refs.loginForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+     if ( refs.inputEmail.value == '' || refs.inputPass.value == '' ) {
+            alert ( 'All fields must be filled' );
+    }
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password =formElements.password.value;
+    const formData = {mail, password};
+    console.log(formData);
+    event.currentTarget.reset();
+}
+
+
