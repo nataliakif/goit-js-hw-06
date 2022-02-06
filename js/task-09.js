63874f904f84f181c07d276e6b7f4d721eba2dbb
nataliakif <nataliakif@gmail.com>
2022-02-06 09:37:17 +0200

    9th

--- js/task-09.js
+++ js/task-09.js
@@ -7,11 +7,11 @@ const refs ={
 refs.changeColorBtn.addEventListener('click', onclickButton);
 
  function onclickButton(event){
-   
-   const getRandomHexColor=()=> `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-
-   refs.body.style.backgroundColor = getRandomHexColor();
-   refs.span.textContent = getRandomHexColor();
+   const changeColor = getRandomHexColor() ;
+   refs.body.style.backgroundColor = changeColor;
+   refs.span.textContent = changeColor;
 }
 
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
